19d4d4364e33ed147aa7632d36ebcd9e41523374
Vineet Patil <vbpatil9980@gmail.com>
2020-04-04 19:33:54 +0530

    Sixth Commit

---
+++ app/src/main/java/com/example/rating/exampleItem.java
@@ -0,0 +1,19 @@
+package com.example.rating;
+
+public class exampleItem {
+    private String mLine1;
+    private String mLine2;
+
+    public exampleItem(String line1, String line2) {
+        mLine1 = line1;
+        mLine2 = line2;
+    }
+
+    public String getLine1() {
+        return mLine1;
+    }
+
+    public String getLine2() {
+        return mLine2;
+    }
+}
