59b5d43bbd4f59a7f0dcd3c342a5a5c7c0f61040
Vineet Patil <vbpatil9980@gmail.com>
2020-04-04 15:44:01 +0530

    Second Commit

--- app/src/main/java/com/example/rating/MainActivity.java
+++ app/src/main/java/com/example/rating/MainActivity.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements
         OnItemSelectedListener{
     Spinner s1,s2;
+    private Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
@@ -22,25 +24,38 @@ public class MainActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_main);
         s1 = (Spinner)findViewById(R.id.spinner1);
         s2 = (Spinner)findViewById(R.id.spinner2);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
         s1.setOnItemSelectedListener(this);
     }
     @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
                                long arg3) {
         // TODO Auto-generated method stub
-        int sp1=Integer.parseInt( String.valueOf(s1.getSelectedItem()));
-        Toast.makeText(this, sp1, Toast.LENGTH_SHORT).show();
+        String sp11=String.valueOf(s1.getSelectedItem());
+        Toast.makeText(this, sp11, Toast.LENGTH_SHORT).show();
+        int sp1 = Integer.parseInt(sp11);
         List<Integer> list = new ArrayList<Integer>();
-        for (;sp1<=10;sp1++){
+        for (;sp1<=9;sp1++){
             list.add(sp1+1);
-            ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<Integer>(this,
-                    android.R.layout.simple_spinner_item, list);
+            ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, list);
             dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             dataAdapter.notifyDataSetChanged();
             s2.setAdapter(dataAdapter);
         }
 
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner 1 : "+ String.valueOf(s1.getSelectedItem()) +
+                                "\nSpinner 2 : "+ String.valueOf(s2.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        } );
     }
+    
     @Override
     public void onNothingSelected(AdapterView<?> arg0) {
         // TODO Auto-generated method stub
